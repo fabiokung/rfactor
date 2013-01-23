467bc78ccfcec6a1dcfe8c280dd49988a38ce867
Rogério Zambon <rogeriozambon@gmail.com>
2013-01-23 10:56:13 -0200

    Change each method

--- lib/rfactor/line_finder.rb
+++ lib/rfactor/line_finder.rb
@@ -3,42 +3,48 @@ module Rfactor
     def initialize(ast)
       @ast = ast
     end
-    
+
     def method_lines(line_in_code)
       processor = MethodLineFinderProcessor.new(line_in_code)
       processor.process(@ast)
       Range.new(processor.method_line, processor.last_method_line)
     end
   end
-  
+
   class MethodLineFinderProcessor < ::SexpProcessor
     attr_reader :method_line
     attr_reader :last_method_line
-    
+
     def initialize(line)
       super()
+
       self.strict = false
       self.require_empty = false
+
       @line = line
       @method_line = 0
       @last_method_line = 0
     end
-    
+
     def process_defn(exp)
       current = exp.line
+
       if current > @method_line && current < @line
         @method_line = current
         @last_method_line = exp.endline
       end
+
       exp
     end
-    
+
     def process_defs(exp)
       current = exp.line
+
       if current > @method_line && current < @line
         @method_line = current
         @last_method_line = exp.last.line
       end
+
       exp
     end
   end
--- lib/rfactor/string_ext.rb
+++ lib/rfactor/string_ext.rb
@@ -1,14 +1,11 @@
 class String
-  
   def last_line
     number = 0
+
     self.each_with_index do |line, i|
       number = i
     end
+
     number + 1
-  end
-  
-  def strip_blank_lines_at_end
-    self.gsub /((\s*)\n)+$/, ''
   end
 end
